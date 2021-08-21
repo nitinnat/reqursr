fb1ee367c630fb8084a8fc95b647b14bcc4cf642
nitinnat <nitinnataraj93@gmail.com>
2021-08-21 05:40:00 -0500

    Updated requirments.yml, added Python 3.6 related fixes and updated setup instructions.

--- README.md
+++ README.md
@@ -1,39 +1,29 @@
 # reqursr - the recursive concept generator
 
-<br>
-To run:
-<ul>
-<li>Install beautifulsoup and flask </li>
-<li> Navigate to the correct directory and run "python app.py" </li>
-<li> Go to localhost:5000 to interact with the application!</li>
-</ul>
+## Setup and Usage
 
-</br>
+1. Create a conda environment using the requirements.yml file. 
+```conda env create --name reqursr --file requirements.yml```
+2. Navigate to flask_app/ and run ```python app.py```
+3. Go to localhost:5000 to interact with the application!
 
-<br>
-Inspiration: Getting into any new subject is a daunting task, especially in ever expanding and rapidly changing fields like machine learning. We have all faced an initial overwhelming option paralysis when starting out, not knowing where to begin or how the different concepts relate to each other. So we wanted to create an application that took any topic as a query and created a concept map of all the important topics and ideas that it was connected to. This not only gives the user a good picture of what the subject entails, but also a roadmap to learning it.
-</br>
+**Note: Requires a valid Google Cloud NLP API key to be added in the code.**
 
-<br>
-What it does: The moment you launch the application, it gives you a plain, google like interface, where there is only a textbox and a button. The user enters any topic of their choice in this search box and presses the 'Go' button. The application then comes up with a visually appealing and intuitive graph of related concepts, abstracting the underlying complexities and providing the user with only the information that is important to them.
-</br>
+##Inspiration 
+Getting into any new subject is a daunting task, especially in ever expanding and rapidly changing fields like machine learning. We have all faced an initial overwhelming option paralysis when starting out, not knowing where to begin or how the different concepts relate to each other. So we wanted to create an application that took any topic as a query and created a concept map of all the important topics and ideas that it was connected to. This not only gives the user a good picture of what the subject entails, but also a roadmap to learning it.
 
-<br>
-How we built it: This application relies on natural language processing (Google's Natural Language API) and web scraping (Wikipedia's API) in order to seek out pages related to the topic and find out all the important entities in these documents. These entities are then put together in a graph structure which is displayed.
-</br>
 
-<br>
-Challenges we ran into: The initial challenge was to consistently extract relevant entities from the web pages related to the topic and select the most relevant ones to be added to the concept graph. The Natural Language API alone was not enough for this task. While it did a great job of identifying the various important entities of any given text, the entities that it picked up were not always relevant to our needs. We had to further prune these entities in order to only select those that had a high chance of being relevant to the topic. Another big challenge was to make all these different technologies work cohesively.
-</br>
+## What it does 
+The moment you launch the application, it gives you a plain, google like interface, where there is only a textbox and a button. The user enters any topic of their choice in this search box and presses the 'Go' button. The application then comes up with a visually appealing and intuitive graph of related concepts, abstracting the underlying complexities and providing the user with only the information that is important to them.
 
-<br>
-Accomplishments that we are most proud of: First of all, we are proud of the fact that we were able to conceive of an idea that we were all passionate about, and deliver a working prototype implementing this idea in a very short period of time. Secondly we are proud of the cross-section of technologies that we ended up using which used the industry best practices, and did not unnecessarily reinvent the wheel, and were able to build on top of what already existed.
-</br>
+## How we built it 
+This application relies on natural language processing (Google's Natural Language API) and web scraping (Wikipedia's API) in order to seek out pages related to the topic and find out all the important entities in these documents. These entities are then put together in a graph structure which is displayed.
 
-<br>
-What's next for ReqursR: There are several fronts on which this application can be further improved. Firstly, in terms of pruning the large list of entities, we propose retraining word2vec to recognize multiple-word entities and provide a similarity score that can be used to remove irrelevant topics. Further, a database backend can be used to reuse previously build graphs which will reduce computation cost and increasingly improve speed the more the application is used. Finally, we can look into improving the user experience by dynamically displaying the graph as it builds to give the user a sense of progress.
-</br>
+## Challenges we ran into
+The initial challenge was to consistently extract relevant entities from the web pages related to the topic and select the most relevant ones to be added to the concept graph. The Natural Language API alone was not enough for this task. While it did a great job of identifying the various important entities of any given text, the entities that it picked up were not always relevant to our needs. We had to further prune these entities in order to only select those that had a high chance of being relevant to the topic. Another big challenge was to make all these different technologies work cohesively.
 
-<br>
-Built with: Flask, Python, Natural Language API, D3.js
-</br>
+## Future Work
+There are several fronts on which this application can be further improved. Firstly, in terms of pruning the large list of entities, we propose retraining word2vec to recognize multiple-word entities and provide a similarity score that can be used to remove irrelevant topics. Further, a database backend can be used to reuse previously build graphs which will reduce computation cost and increasingly improve speed the more the application is used. Finally, we can look into improving the user experience by dynamically displaying the graph as it builds to give the user a sense of progress.
+
+## Technologies
+Flask, Python, Natural Language API, D3.js
--- flask_app/app.py
+++ flask_app/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, jsonify
+
 # -*- coding: utf-8 -*-
 """
 Created on Sat Jan 20 00:07:25 2018
 
 @author: Nitin
 """
-
 
 from bs4 import BeautifulSoup
 import requests
@@ -15,38 +15,46 @@ import os
 
 from flask import render_template, redirect, url_for, request
 from nltk import pos_tag
+
 base_link = "https://en.wikipedia.org"
 link = "/wiki/Computer_vision"
 wiki_api_link = "https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search="
 wiki_title_link = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles="
+
+
 def google_nlp(text):
     base = "https://language.googleapis.com"
     doc = {"type": "PLAIN_TEXT", "content": text}
     request_data = {"document": doc, "encodingType": "UTF8"}
-    
+
     entities_endpoint = "/v1/documents:analyzeEntities"
     entities_url = base + entities_endpoint + "?key=AIzaSyAGy5__54Qd0u5wEUC5uOIFCVBYc1hleQc"
-    
+
     response = requests.post(entities_url, data=json.dumps(request_data))
     entities = json.loads(response.text)
-    return [entities["entities"][i]['name'] for i in range(len(entities["entities"])) if 
-          entities["entities"][i]['type'] == 'OTHER']
+    print(entities)
+    return [entities["entities"][i]['name'] for i in range(len(entities["entities"])) if
+            entities["entities"][i]['type'] == 'OTHER']
+
+
 def scrape_extract(title):
     my_bytes_value = requests.get(wiki_api_link + title).content
     return scrape_wiki_link(json.loads(my_bytes_value)[3][0])
 
+
 def get_first_link(query):
     return json.loads(requests.get(wiki_api_link + query).content)
+
 
 def scrape_wiki_link(link):
     temppage = requests.get(link)
     tempsoup = BeautifulSoup(temppage.content, 'html.parser')
-    
-    #print(tempsoup.get_text())
-    p =  tempsoup.find_all('p')
+
+    # print(tempsoup.get_text())
+    p = tempsoup.find_all('p')
     title = tempsoup.find_all('title')
     text = ''
-    
+
     for wrapper in p[0:4]:
         print("Mowing through p tags...")
         """
@@ -56,106 +64,98 @@ def scrape_wiki_link(link):
                  (not "Wikipedia:Citation_needed" in c["href"])): 
                 child_info.append((c['href'],c.string))
         """
-        text = text +' ' + wrapper.get_text()
-    
-    
-    return title,text
+        text = text + ' ' + wrapper.get_text()
+
+    return title, text
 
 
 ##Test
-    
-#a = scrape_wiki_link(base_link +link)[1]
+
+# a = scrape_wiki_link(base_link +link)[1]
 
 def BFS(query):
     query = query.lower()
     start_link = json.loads(requests.get(wiki_api_link + query).content)[3][0]
     count = 0
     queue = []
-    links = [] 
-    nodes = {} #name - group pair
+    links = []
+    nodes = {}  # name - group pair
     s = (query.lower(), start_link)
     group_num = 0
     parent_num = 0
-    nodes[unicode(query.lower())] = group_num
+    nodes[str(query.lower())] = group_num
     queue.append(s)
-    
 
-    
-    
     while queue and count <= 1:
         print(count)
         s = queue.pop(0)
-        
-        if s[1] != None and '#' not in s[1]:            
-            title, text= scrape_wiki_link(s[1])
-            
-            #Add this entity to the nodes list if it doesn't exist
+
+        if s[1] is not None and '#' not in s[1]:
+            title, text = scrape_wiki_link(s[1])
+
+            # Add this entity to the nodes list if it doesn't exist
             if not s[0].lower() in nodes.keys():
                 print("Adding again")
                 group_num += 1
                 nodes[s[0].lower()] = group_num
-                
-            
-            #Add link
-            temp_link = {"source" : nodes[s[0].lower()], "target": group_num,"value": 1}
+
+            # Add link
+            temp_link = {"source": nodes[s[0].lower()], "target": group_num, "value": 1}
             if not temp_link in links:
                 links.append(temp_link)
-                
-            
-            #Find entities
+
+            # Find entities
             entities = google_nlp(text)
-            #Pos tag it as well
-            
-            #Remove duplicates and convert to lowercase
+            # Pos tag it as well
+
+            # Remove duplicates and convert to lowercase
             entities = list(set(ent.lower() for ent in entities))
-            
-            
-            #If entities exist, then append to queue
+
+            # If entities exist, then append to queue
             if entities:
-                
+
                 for ent in [e for e in entities if not e in nodes.keys()]:
                     try:
                         temp_link = json.loads(requests.get(wiki_api_link + ent).content)[3][0]
-                        queue.append((ent,temp_link))
-                        
-                        #Add to nodes
+                        queue.append((ent, temp_link))
+
+                        # Add to nodes
                         if not ent in nodes.keys():
                             group_num += 1
                             nodes[ent.lower()] = group_num
-                            #print("Added %s as %d"%(ent,group_num))
-                            #Add to links
-                        temp_link = {"source" : nodes[s[0].lower()], "target": group_num,"value": 1}
+                            # print("Added %s as %d"%(ent,group_num))
+                            # Add to links
+                        temp_link = {"source": nodes[s[0].lower()], "target": group_num, "value": 1}
                         if not temp_link in links:
                             links.append(temp_link)
                     except IndexError:
                         continue
-                
+
                 count += 1
-            
-    return queue,nodes,links
+
+    return queue, nodes, links
+
 
 def get_graph_data(query):
-    queue, nodes,links = BFS(query.lower())
+    queue, nodes, links = BFS(query.lower())
     node_list = []
     for n in nodes.keys():
         print("Scraping from links...")
-        node_list.append({"id":n,"group":nodes[n], "text": scrape_extract(n)[1]})
+        node_list.append({"id": n, "group": nodes[n], "text": scrape_extract(n)[1]})
     my_json = {"nodes": node_list, "links": links}
     with open('graph.json', 'w') as fp:
         json.dump(my_json, fp)
     return my_json
-    
-        
 
 
 app = Flask(__name__)
 
 
-@app.route("/data/<query>",methods=['GET','POST'])
+@app.route("/data/<query>", methods=['GET', 'POST'])
 def data(query):
     response = get_graph_data(query)
-    
-    print(query,type(query))
+
+    print(query, type(query))
     return jsonify(response)
 
 
@@ -163,7 +163,8 @@ def data(query):
 def index():
     return render_template("index.html")
 
-@app.route("/viz/<num>",methods=['GET','POST'])
+
+@app.route("/viz/<num>", methods=['GET', 'POST'])
 def viz(num):
     return render_template("viz.html")
 
